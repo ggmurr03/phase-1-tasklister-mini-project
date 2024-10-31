ca54e4b73897c8e52540722a418d9b2dc7ea64b1
Gregory Murray <gr.j.murray@gmail.com>
2024-10-31 03:27:27 -0700

    a

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+// As a user, I should be able to type a task into the input field.
+// As a user, I should be able to click some form of a submit button.
+// As a user, I expect to see the task string that I provided appear in the DOM after the submit button has been activated.
+
+{/* <form id="create-task-form" action="" method="POST">
+<label for="new-task-description">Task description:</label>
+<input type="text" id="new-task-description" name="new-task-description" placeholder="description">
+<input type="submit" value="Create New Task">
+</form> */}
+
+formElement = document.querySelector("form")
+taskText = formElement.querySelector("#new-task-description")
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  formElement.addEventListener("submit", () => {
+    event.preventDefault();
+  })
+  taskText.addEventListener("submit",()=> console.log(nameInput.value))
 });
+
+
+event.preventDefault()
+console.log()
